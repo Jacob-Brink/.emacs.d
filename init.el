3b3bc1d82f8801c90c68853acd83997aa6e0152f
Jacob Brink <jacobpeterbrink@gmail.com>
2020-11-01 09:09:13 -0500

    Revert "broken:"
    This reverts commit 16d158576ada4d58f5587825c003cb3db853f0c0.

--- init.el
+++ init.el
@@ -11,10 +11,12 @@
  '(ansi-color-names-vector
    ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
  '(custom-safe-themes
-   '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "8e79884e89740cf6b7e0210f52e4ac995dc1f1a9a17151bfcfb4d660757a011b" "bcd0237b2a5b7897e482458cc62c4f3fa3d9d7f9a9667338e67d4c7a8e009819" "454c1c9ce70f7d807c51c890910365fd3c64a9e63f596511e9ff57dd97bbeea8" default))
+   (quote
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "8e79884e89740cf6b7e0210f52e4ac995dc1f1a9a17151bfcfb4d660757a011b" "bcd0237b2a5b7897e482458cc62c4f3fa3d9d7f9a9667338e67d4c7a8e009819" "454c1c9ce70f7d807c51c890910365fd3c64a9e63f596511e9ff57dd97bbeea8" default)))
  '(fci-rule-color "#eeeeee")
  '(hl-todo-keyword-faces
-   '(("TODO" . "#dc752f")
+   (quote
+    (("TODO" . "#dc752f")
      ("NEXT" . "#dc752f")
      ("THEM" . "#2d9574")
      ("PROG" . "#4f97d7")
@@ -28,11 +30,12 @@
      ("TEMP" . "#b1951d")
      ("FIXME" . "#dc752f")
      ("XXX+" . "#dc752f")
-     ("\\?\\?\\?+" . "#dc752f")))
+     ("\\?\\?\\?+" . "#dc752f"))))
  '(org-export-with-toc 10)
  '(package-selected-packages
-   '(eziam-theme rjsx-mode neotree use-package spacemacs-theme org-ref org-noter org-journal magit hackernews eyebrowse))
- '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e"))
+   (quote
+    (eziam-theme rjsx-mode neotree use-package spacemacs-theme org-ref org-noter org-journal magit hackernews eyebrowse)))
+ '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e")))
  '(vc-annotate-background "#eeeeee"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
